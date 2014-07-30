ca09d960c6f45c526d8e151026605f4d216920a2
natalieparellano <natalie.p.arellano@gmail.com>
2014-07-30 11:09:54 -0700

    On the Stop model, setup to_transfers and from_transfers associations using Transfer as the join table

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -5,6 +5,9 @@ class Stop < ActiveRecord::Base
   has_many :trips, through: :stop_times
   has_many :subway_routes, through: :trips
 
+  has_many :to_transfers, foreign_key: "to_stop_id", class_name: "Transfer"
+  has_many :from_transfers, foreign_key: "from_stop_id", class_name: "Transfer"
+
   def next_departing_train(route, datetime)
     stop_times.next_departing_train(self, route, datetime)
   end 
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,3 +1,4 @@
 class Transfer < ActiveRecord::Base
-  
+  # belongs_to :from_stop
+  # belongs_to :to_stop
 end 
