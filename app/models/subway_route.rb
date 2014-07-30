663f720b89ea9d15d6f18a1dec48d5c952e58581
natalieparellano <natalie.p.arellano@gmail.com>
2014-07-30 11:41:02 -0700

    stop_times and transfers don't have primary keys -- this is a problem for calling uniq

--- app/models/subway_route.rb
+++ app/models/subway_route.rb
@@ -1,6 +1,6 @@
 class SubwayRoute < ActiveRecord::Base
   self.primary_key = "route_id"
-  has_many :trips, foreign_key: :route_id
+  has_many :trips, foreign_key: :route_id # this is needed so that the query looks for trips.route_id instead of trips.subway_route_id
 
   has_many :stop_times, through: :trips
   has_many :stops, through: :stop_times
